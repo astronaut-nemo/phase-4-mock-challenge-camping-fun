b0e82338035c093c0deda7c58c75fa9cfcbcfa9f
Joanna <astronautnemo@gmail.com>
2023-03-22 13:46:44 +0300

    added the Camper Route POST /campers

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,6 @@
 class CampersController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     # GET /campers
     def index
@@ -14,10 +15,21 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_respons
     end
 
     # POST /campers
-
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
+    end
 
     private
+    def camper_params
+        params.permit(:name, :age)
+    end
+
     def render_record_not_found_response
         render json: {error: "Camper not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
